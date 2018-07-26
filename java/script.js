aa09afce277de000c9afd68ed1c84a4093bec82b
Kalpit <kalpitdwivedi28.github.com>
2018-07-26 10:14:28 +0530

    Sevencolors is simple school website

---
+++ java/script.js
@@ -0,0 +1,55 @@
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 600) {
+	    $(".welcomeimages_01").fadeOut(5000);
+	  }
+
+	  else{
+		  $(".welcomeimages_01").fadeIn(3000);  	
+	  }
+  })
+})
+
+/********************************************************************/
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 600) {
+	    $(".welcomeimages_02").fadeOut(7000);
+	  }
+
+	  else{
+		  $(".welcomeimages_02").fadeIn(5000);  	
+	  }
+  })
+})
+
+/******************************************************************/
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 600) {
+	    $(".welcomeimages_03").fadeOut(5000);
+	  }
+
+	  else{
+		  $(".welcomeimages_03").fadeIn(3000);  	
+	  }
+  })
+})
+
+/******************************************************************/
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 800) {
+	    $(".ourpic").fadeOut(5000);
+	  }
+
+	  else{
+		  $(".ourpic").fadeIn(3000);  	
+	  }
+  })
+})
